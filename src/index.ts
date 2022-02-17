1a00b858ca21a9695483c8e49dd276c83e24c3ae
vanessa gaete <vanessagaete@MacBook-Air-de-vanessa.local>
2022-02-17 18:02:47 -0300

    Cambio en la sintaxis y tokens

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,30 @@ import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, d
 import {styleTags, tags as t} from "@codemirror/highlight"
 
 export const EXAMPLELanguage = LRLanguage.define({
-  parser: parser.configure({
-    props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
-      styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {line: ";"}
+    parser: parser.configure({
+      props: [
+        indentNodeProp.add({
+          Application: delimitedIndent({closing: ")", align: false})
+        }),
+        foldNodeProp.add({
+          Application: foldInside
+        }),
+        styleTags({
+          Type: t.className,
+          Attr: t.variableName,
+          String: t.string,
+          Keyword: t.keyword,
+          Query: t.keyword,
+          LineComment: t.lineComment,
+          "{ }": t.paren
+        })
+      ]
+    }),
+    languageData: {
+      commentTokens: {line: ""}
+    }
+  })
+  
+  export function EXAMPLE() {
+    return new LanguageSupport(EXAMPLELanguage)
   }
-})
-
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
-}
